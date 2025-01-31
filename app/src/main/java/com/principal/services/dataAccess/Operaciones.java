90c02b94dcfc3a4a2e6d3d805ed97781c49deab3
Starlap Software <Starlap.software@hotmail.com>
2025-01-31 17:25:40 -0500

    2025.01.31.f.1

--- app/src/main/java/com/principal/services/dataAccess/Operaciones.java
+++ app/src/main/java/com/principal/services/dataAccess/Operaciones.java
@@ -2,6 +2,8 @@ package com.principal.services.dataAccess;
 
 import android.util.Log;
 
+import com.google.gson.Gson;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,6 +28,7 @@ public class Operaciones {
 
     public  void getUsuario() throws JSONException {
         // create your json here
+        UsuarioService usuario=null;
         JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("Username", "facturacionmadamelili@gmail.com");
@@ -57,11 +60,86 @@ public class Operaciones {
                 public void onResponse(Call call, Response response) throws IOException {
                     if(response.isSuccessful())
                     {
-                        String resStr = response.body().string();
+                        try
+                        {
+                            String jsonData = response.body().string();
+                            Gson gson=new Gson();
+                             UsuarioService user=gson.fromJson(jsonData,UsuarioService.class);
+                            Log.i("Body res", user._NombreEmpresa);
+                            getVendedores(user);
+
+                        } catch (Exception e)
+                        {
+                            Log.i("Body res",e.toString());
+                        }
                     }
                 }
             });
             String resStr = response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+
+
+
+        } catch (Exception e) {
+            Log.i("Error2****", e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    public  void getVendedores(UsuarioService usuario) throws JSONException {
+        // create your json here
+        JSONObject jsonObject = new JSONObject();
+        try {
+           // jsonObject.put("Username", "facturacionmadamelili@gmail.com");
+            jsonObject.put("", "");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        OkHttpClient client = new OkHttpClient();
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        // put your json here
+        RequestBody body = RequestBody.create(JSON, jsonObject.toString());
+        Request request = new Request.Builder()
+                .url("http://192.168.20.246:84/api/Finance/GetVendedoresWS")
+                .addHeader("Authorization", "Bearer " + usuario.get_token())
+                .post(body)
+                .build();
+
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+
+            /**
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if(response.isSuccessful())
+                    {
+                        try
+                        {
+                            String jsonData = response.body().string();
+                            Log.i("Body res", jsonData);
+
+                        } catch (Exception e)
+                        {
+                            Log.i("Body res",e.toString());
+                        }
+                    }
+                }
+            });
+             */
+            Log.i("Body res",response.body().string());
+            //String resStr =response.body().string();
         } catch (IOException e) {
             e.printStackTrace();
         }
